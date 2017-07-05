043c2bbef6576ab7ec73f13e908c2f432589d38f
taejaehan <hantaejae@gmail.com>
2017-07-06 00:35:12 +0900

    add velocity example

--- entry.js
+++ entry.js
@@ -40,6 +40,83 @@ import * as d3 from 'd3';
 const square = d3.selectAll("rect");
 square.style("fill", "orange");
 
+window.onload = function () {
+
+var canvas = document.getElementById('defaultCanvas0'),
+  context = canvas.getContext('2d');
+
+function Circle() {
+  var self = this;
+  this.init = function() {
+    self.pos = [0, 0];
+    self.vel = [0, 0];
+    self.acc = [0, 0];
+    self.wind = [0.1, 0];
+    self.gravity = [0 ,0.1];
+    self.mass = 1;
+    self.radius = 5;
+  };
+  this.update = function() {
+    //4. '힘'을 계산하여 '가속도'에 더해준다.
+    //바람 +0.1, 중력 +0.1의 힘을 계속 주고 있다.
+    var force = [
+      self.wind[0] + self.gravity[0],
+      self.wind[1] + self.gravity[1]
+    ];
+    //A = F / M 이지만 질량은 무시(1로 계산)
+    self.acc[0] += force[0] / self.mass;
+    self.acc[1] += force[1] / self.mass;
+    //3. '속도'는 '가속도'로 매 프레임마다 변화시킨다.
+    self.vel[0] += this.acc[0];
+    self.vel[1] += this.acc[1];
+    //2. '위치'는 '속도'벡터로 매 프레임마다 변화시킨다.
+    self.pos[0] += this.vel[0];
+    self.pos[1] += this.vel[1];
+  };
+  this.checkEdges = function(){
+    var halfRadius = self.radius / 2;
+    if(self.pos[0] > canvas.width - halfRadius){
+      self.pos[0] = canvas.width - halfRadius;
+      self.vel[0] = -(self.vel[0]);
+    }else if(self.pos[0] < halfRadius){
+      self.pos[0] = halfRadius;
+      self.vel[0] = -(self.vel[0]);
+    }
+    
+    if(self.pos[1] > canvas.height - halfRadius){
+      self.pos[1] = canvas.height - halfRadius;
+      self.vel[1] = -(self.vel[1]);
+    }else if(self.pos[1] < halfRadius){
+      self.pos[1] = halfRadius;
+      self.vel[1] = -(self.vel[1]);
+    }
+  };
+  this.draw = function() {
+    context.beginPath();
+    //1. 오브젝트의 '위치'를 매 프레임마다 그려준다.
+    context.arc(self.pos[0], self.pos[1], self.radius, 0, Math.PI*2, false); 
+    context.closePath();
+    context.fill();
+    //5. '가속도'를 매 프레임마다 초기화시킨다.
+    self.acc = [0,0];
+  };
+};
+
+var obj = new Circle();
+obj.init();
+// animation loop
+function loop() {
+  context.clearRect(0,0,canvas.width,canvas.height);
+  obj.update();
+  obj.checkEdges();
+  obj.draw();
+  requestAnimationFrame(loop);
+  console.log('loop');
+}
+
+// loop();
+};
+
 // var p5 = require('p5');
 
 // function sketch(p) {
